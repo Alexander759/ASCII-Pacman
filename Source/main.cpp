b4e619891ccc8c74a8f7a49f668f1ec8bb7cf177
Alexander759 <aleks.asenov2005@gmail.com>
2025-01-15 10:29:38 +0200

    Add game structs and game modes

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,70 @@
 #include <iostream>
 
+#pragma region GameDataStructures
+
+enum GameMode {
+	ChaseMode,
+	FrightenedMode
+};
+
+struct VectorMovement;
+struct Coordinates;
+struct Map;
+struct Pacman;
+struct Ghost;
+struct Game;
+
+struct VectorMovement {
+	int x;
+	int y;
+};
+
+struct Coordinates {
+	int x;
+	int y;
+};
+
+struct Map {
+	char** content;
+	int verticalSize;
+	int horizontalSize;
+};
+
+struct Ghost {
+	Coordinates position;
+	Coordinates reappearCoordinates;
+	Coordinates target;
+	void (*setTarget) (Game& game);
+	const VectorMovement* currentVector;
+
+	int chaseModeColorCode;
+	char symbol;
+	char previousSymbolOnCurrentPosition;
+};
+
+struct Pacman {
+	Coordinates position;
+	const VectorMovement* currentDirection;
+	int pacmanColor;
+	char symbol;
+};
+
+struct Game {
+	Ghost blinky;
+	Ghost pinky;
+	Ghost inky;
+	Ghost clyde;
+	Pacman pacman;
+	Map map;
+	GameMode currentGameMode;
+
+	int foodCount;
+	int leftInFrightenedMode;
+	int score;
+};
+
+#pragma endregion
+
 int main()
 {
-    std::cout << "Hello World!\n";
 }
