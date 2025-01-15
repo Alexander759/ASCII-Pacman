56179710c558c9101d94d8609db6dd25f22eb483
Alexander759 <aleks.asenov2005@gmail.com>
2025-01-15 12:19:25 +0200

    Add set up for pacman in game

--- Source/main.cpp
+++ Source/main.cpp
@@ -73,10 +73,40 @@ struct Game {
 
 const int MAZEMAXSIZE = 50;
 
+const char PACMANSYMBOL = 'Y';
+const int PACMANCOLORCODE = 14;
+
+const char FOODSYMBOL = '-';
+const int FOODCOLORCODE = 14;
+
+const char PELLETSYMBOL = '@';
+const int PELLETCOLOR = 14;
+
+const char FREESPACESYMBOL = ' ';
+
+const int NUMBERSOFSYMBOLSFORFOOD = 2;
+char SYMBOLSFORFOOD[NUMBERSOFSYMBOLSFORFOOD] = { FOODSYMBOL, PELLETSYMBOL };
+
 char MAPFILENAME[] = "map.txt";
 
 char UNABLETOOPENFILEMESSAGE[] = "Couldn't open file!";
 char FILETOOSHORTMESSAGE[] = "File shorter than expected!";
+
+const int VECTORSLENGTH = 5;
+
+const VectorMovement ZEROVECTOR = { 0,0 };
+const VectorMovement UPVECTOR = { 0,-1 };
+const VectorMovement LEFTVECTOR = { -1,0 };
+const VectorMovement DOWNVECTOR = { 0,1 };
+const VectorMovement RIGHTVECTOR = { 1,0 };
+
+const VectorMovement* vectors[VECTORSLENGTH] = {
+	&ZEROVECTOR,
+	&UPVECTOR,
+	&LEFTVECTOR,
+	&DOWNVECTOR,
+	&RIGHTVECTOR
+};
 
 #pragma endregion
 
@@ -95,7 +125,38 @@ int getStringLength(const char* str) {
 
 	return count;
 }
+bool isPacman(char symbol) {
+	return symbol == PACMANSYMBOL;
+}
 
+bool isRegularFood(char symbol) {
+	return symbol == FOODSYMBOL;
+}
+
+bool isPellet(char symbol) {
+	return symbol == PELLETSYMBOL;
+}
+
+bool isFood(char symbol) {
+	for (size_t i = 0; i < NUMBERSOFSYMBOLSFORFOOD; i++)
+	{
+		if (SYMBOLSFORFOOD[i] == symbol) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+void showConsoleCursor(bool showCursor) {
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursorInfo;
+
+	GetConsoleCursorInfo(hStdout, &cursorInfo);
+	cursorInfo.bVisible = showCursor;
+	SetConsoleCursorInfo(hStdout, &cursorInfo);
+}
 #pragma endregion
 
 #pragma region MapFunctions
@@ -104,7 +165,6 @@ bool areCoordinatesInMapRange(Map& map, const Coordinates& coordinates) {
 	return coordinates.x >= 0 && coordinates.y >= 0
 		&& map.horizontalSize > coordinates.x && map.verticalSize > coordinates.y;
 }
-
 
 char getAtPosition(Map& map, const Coordinates& coordinates) {
 	if (!areCoordinatesInMapRange(map, coordinates)) {
@@ -122,6 +182,24 @@ void setAtPosition(Map& map, const Coordinates& coordinates, char symbol) {
 
 	map.content[coordinates.y][coordinates.x] = symbol;
 }
+
+void getPositionOfSymbol(Map& map, Coordinates& result, char symbol) {
+	for (size_t i = 0; i < map.verticalSize; i++)
+	{
+		for (size_t j = 0; j < map.horizontalSize; j++)
+		{
+			if (map.content[i][j] == symbol) {
+				result.x = j;
+				result.y = i;
+				return;
+			}
+		}
+	}
+
+	result.x = -1;
+	result.y = -1;
+}
+
 
 void disposeMap(Map& map) {
 	for (size_t i = 0; i < map.verticalSize; i++)
@@ -206,11 +284,50 @@ void readMap(Map& map, const char* fileName) {
 
 #pragma region SetUpGame
 
+int countFood(Map& map) {
+	int count = 0;
+	for (int i = 0; i < map.verticalSize; i++)
+	{
+		for (int j = 0; j < map.horizontalSize; j++)
+		{
+			if (isFood(getAtPosition(map, { j, i }))) {
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
+
+bool setUpPacman(Game& game) {
+	game.pacman.symbol = PACMANSYMBOL;
+	getPositionOfSymbol(game.map, game.pacman.position, game.pacman.symbol);
+	game.pacman.currentDirection = &ZEROVECTOR;
+	game.pacman.pacmanColor = PACMANCOLORCODE;
+	game.foodCount = countFood(game.map);
+	game.currentGameMode = ChaseMode;
+	return true;
+}
+
+
+bool setUpGame(Game& game) {
+	showConsoleCursor(false);
+
+	game.leftInFrightenedMode = 0;
+	game.score = 0;
+	readMap(game.map, MAPFILENAME);
+
+	if (game.map.horizontalSize == 0 || game.map.verticalSize == 0) {
+		return false;
+	}
+
+	return setUpPacman(game);
+}
+
 #pragma endregion
-
-
 
 int main()
 {
-	
+	Game game;
+	setUpGame(game);
 }
